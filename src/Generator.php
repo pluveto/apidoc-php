d887f3c44ff11d3af99c0126032da8d050aa851f
Pluveto <i@pluvet.com>
2020-02-13 16:26:59 +0800

    update

--- src/Generator.php
+++ src/Generator.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * API 文档生成器 (开发中)
  * @author Pluveto <i@pluvet.com> 2020-2-13
@@ -8,6 +9,7 @@ namespace ApiDoc;
 
 define('DEBUG', true);
 
+// 循环遍历文件夹
 function rglob($pattern, $flags = 0)
 {
     $files = glob($pattern, $flags);
@@ -17,39 +19,56 @@ function rglob($pattern, $flags = 0)
     return $files;
 }
 
-class Generator{
-    public static function run($event){
+class Generator
+{
+    public static function run($event)
+    {
         $argv = $event->getArguments();
-        if(DEBUG){
+        if (DEBUG) {
             var_dump($argv);
         }
         $arglist = [];
-        foreach($argv as $arg){
+        foreach ($argv as $arg) {
             $exp = explode("=", $arg, 2);
             $arglist[trim($exp[0])] = $exp[1];
         }
-        foreach (rglob($arglist['in'].'/*.php') as $file) {
-            require_once $file;
-            // get the file name of the current file without the extension
-            // which is essentially the class name
-            $classBase = "\\App" . substr(str_replace("/", "\\", dirname($file)), 3);
-        
-            $className = basename($file, '.php');
-            $fullClassName = $classBase . "\\" . $className;
-            echo "found file: $file\n";
-            if (!class_exists($fullClassName)) {
-                continue;
-            }
-            echo "----->found class: \e[1;36m$fullClassName\e[0m\n";
-            fwrite($fRoute, "\$api$className = new $fullClassName();\n");
-            $reflector = new \ReflectionClass($fullClassName);
-            $functions = $reflector->getMethods(\ReflectionMethod::IS_PUBLIC);
-            for ($i = 0; $i < count($functions); $i++) {
-                $function = $functions[$i];
-                $functionName = $function->getName();
-                echo "------->found api: \e[1;35m$functionName\e[0m ";
-                $comment = $function->getDocComment();
+
+        if (!array_key_exists("in", $arglist)) {
+            $arglist['in'] = ".";
+        }
+
+        foreach (rglob($arglist['in'] . '/*.php') as $file) {
+            $blockDocList = \ApiDoc\Language\Classic::GetBlockDoc(realpath($file));
+            foreach ($blockDocList as $block) {
+                $api = self::processBlock($block);
+                var_dump($api);
             }
         }
+    }
+    public static function processBlock($linesBlock)
+    {
+        $ret = [];
+        foreach ($linesBlock as $line) {
+            $re = '/^\@(.*?) /';
+            preg_match($re, $line["value"], $matches);
+            if (!$matches) {
+                continue;
+            }
+
+            $annotionType = ucfirst($matches[1]);
+            $className = "ApiDoc\\Parser\\$annotionType";
+            if (!Class_exists($className)) {
+                if (DEBUG) {
+                    // eho "Class $className doesn't exist\n";
+                }
+                continue;
+            }
+
+            $line["value"] = ltrim(mb_substr($line["value"], 1 + mb_strlen($matches[1])));
+            $parsed = call_user_func("$className::parse", $line);
+            $ret = array_merge($ret, $annotionType === "ApiParam" ?
+                ["fields" => [$parsed]] : $parsed);
+        }
+        return $ret;
     }
 }
